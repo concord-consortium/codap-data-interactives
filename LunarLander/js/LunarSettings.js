ae0dbb8b2251307126a8e3020ee33fbe68a88f03
Evangeline Ireland <eireland@concord.org>
2014-09-15 16:25:39 -0700

    Moving Lunar Lander from dg-app to PostMessage API

---
+++ LunarLander/js/LunarSettings.js
@@ -0,0 +1,32 @@
+// ==========================================================================
+// Project:   LunarLander
+// Copyright: ©2012 KCP Technologies, Inc.
+// ==========================================================================
+
+/**
+ * @fileoverview Defines LunarSettings, the parameters that determine most of the game characteristics
+ * @author bfinzer@kcptech.com (William Finzer)
+ * @preserve (c) 2012 KCP Technologies, Inc.
+ */
+
+var LunarSettings = {
+  kTickInterval: 100,  //milliseconds
+  defaultNames: {
+    left: {
+      craft: 'S.S. Dolphin',
+      pilot: 'Captain Olimar',
+      keyTop: 'w',
+      keyBottom: 's',
+      topColor: 'silver',
+      bottomColor: 'silver'
+    },
+    right: {
+      craft: 'The Ship',
+      pilot: 'Louie',
+      keyTop: 'o',
+      keyBottom: 'l',
+      topColor: 'red',
+      bottomColor: 'red'
+    }
+  }
+};
