7347773655beb7378a0d96888d2857361733b2e5
Jonathan Sandoe <jsandoe@concord.org>
2016-04-27 08:27:14 -0700

    Begin to separate the data model.

--- DataCard/DataCardR.js
+++ DataCard/DataCardR.js
@@ -16,175 +16,14 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 // ==========================================================================
-
-var ContextSelector = React.createClass({
-  render: function () {
-    var options =
-        this.props.contexts.map(function (name) {
-          return (
-              <option key={name} > {name} </option>
-          );
-        });
-    return <select id="context-selector" >
-        {options} </select>
-
-  }
-});
-
-var ContextMenu = React.createClass({
-  render: function () {
-    return <section id="context-selector">
-      <label>Context:&nbsp;
-        <ContextSelector contexts={this.props.contexts} />
-      </label >
-    </section>
-  }
-});
-
-var AttrList = React.createClass({
-  render: function () {
-    var items = this.props.attrs.map(function (item) {
-      return <div key={item.name} className="attr">{item.title}</div>
-    });
-    return <div className="attr-list">{items}</div>
-  }
-});
-
-var CaseDisplay = React.createClass ({
-  render: function () {
-    var myCase = this.props.myCase;
-    var values = this.props.attrs.map(function (attr) {
-      return <div className="attr-value" key={attr.name}> {myCase.values[attr.name]} </div>;
-    });
-    return <div className="case">{values}</div>;
-  }
-});
-
-var CaseList = React.createClass({
-  render: function () {
-    var caseIndex = this.props.collection.currentCaseIndex || 0;
-    var myCase = this.props.collection.cases[caseIndex];
-    var caseView = <CaseDisplay key={myCase.id} attrs={this.props.collection.attrs} myCase={myCase}/>;
-    return <div className="case-container"> {caseView} </div>;
-  }
-});
-
-var CaseNavControl = React.createClass({
-  symbol: {
-    left: '<',
-    right: '>'
-  },
-  handleClick(ev) {
-    this.props.onNavigation && this.props.onNavigation(this.props.action);
-  },
-  render: function () {
-    return <div className="control" onClick={this.handleClick}>{this.symbol[this.props.action]}</div>
-  }
-});
-
-var DataCard = React.createClass({
-  moveCard: function (direction) {
-    console.log('moveCard: direction: ' + direction);
-    this.props.onNavigation && this.props.onNavigation(this.props.collection.name, direction);
-  },
-  render: function () {
-    return <section className="card-section">
-      <div className="collection-name">{this.props.collection.title}</div>
-      <div className="card-deck">
-        <div className="left-ctls">
-          <CaseNavControl action="left" onNavigation={this.moveCard} />
-        </div>
-        <div className="attr-container">
-          <AttrList attrs={this.props.collection.attrs} />
-        </div>
-        <div className="case-frame">
-          <CaseList collection={this.props.collection} />
-        </div>
-        <div className="right-ctls">
-          <CaseNavControl action="right"  onNavigation={this.moveCard}/>
-          <div className="control ctl-add-case">+</div>
-        </div>
-      </div>
-    </section>
-  }
-});
-
-var DataCardApplication = React.createClass({
-  moveCard: function (collectionName, direction) {
-    function adjustParentCard(collectionIx, myCase) {
-      if (collectionIx <= 0) {
-        return;
-      }
-      var parentId = myCase.parentId;
-      var parentCollection = collections[collectionIx - 1];
-      if (parentCollection.cases[parentCollection.currentCaseIndex].id === parentId) {
-        return;
-      }
-      var parentCaseIndex = parentCollection.cases.findIndex(function(iCase) {
-        return iCase.id === parentId;
-      });
-      console.log('adjustParentCard. ' + JSON.stringify({
-            parentId: parentId,
-            parentCollectionCaseIds: parentCollection.cases.map(function (iCase) {return iCase.id}),
-            collectionIx: collectionIx,
-            parentCaseIndex: parentCollection.currentCaseIndex
-          }));
-      if (parentCaseIndex >= 0) {
-        parentCollection.currentCaseIndex = parentCaseIndex;
-      }
-      adjustParentCard(collectionIx - 1, parentCollection.cases[parentCollection.currentCaseIndex])
-    }
-    function adjustChildCard(collectionIx, myCase){
-      if (collectionIx + 1 >= collections.length) {
-        return;
-      }
-      var parentId = myCase.id;
-      var childCollection = collections[collectionIx + 1];
-      var childCase  = childCollection.cases[childCollection.currentCaseIndex];
-      var childCaseIx;
-      if (childCase.parentId !== parentId) {
-        childCaseIx = childCollection.cases.findIndex(function (iCase) { return iCase.parentId === parentId; });
-        console.log('adjustChildCard. ' + JSON.stringify({
-              parentId: parentId,
-              childCollectionCaseIds: childCollection.cases.map(function (iCase) {return iCase.id}),
-              collectionIx: collectionIx,
-              childCaseIndex: childCaseIx
-            }));
-        if (childCaseIx >= 0) {
-          childCollection.currentCaseIndex = childCaseIx;
-        }
-        adjustChildCard(collectionIx + 1, childCollection.cases[childCollection.currentCaseIndex]);
-      }
-    }
-    var newCaseIx = null;
-    var collections = this.data.collections;
-    var collectionIx = collections.findIndex(function (collection) {
-      return collection.name === collectionName;
-    });
-    var collection = (collectionIx >= 0) && this.data.collections[collectionIx];
-
-    console.log('moveCard. ' + JSON.stringify({
-          collection: collectionName,
-          direction: direction,
-          collectionIx: collectionIx,
-          length: collection.cases.length,
-          currentCaseIndex: collection.currentCaseIndex
-        }));
-    if (collection) {
-      if (direction === 'left' && collection.currentCaseIndex > 0) {
-        newCaseIx = collection.currentCaseIndex = collection.currentCaseIndex - 1;
-      } else if (direction === 'right' && (collection.currentCaseIndex + 1) < collection.cases.length) {
-        newCaseIx = collection.currentCaseIndex = collection.currentCaseIndex + 1;
-      } else {
-        return;
-      }
-      adjustParentCard(collectionIx, collection.cases[newCaseIx]);
-      adjustChildCard(collectionIx, collection.cases[newCaseIx]);
-      this.setState(this.data);
-    }
-  },
+var dataManager = Object.create({
+  setState: null,
   data: {
-    contexts: ['BartStations', 'BartRoutes', 'BartArrivals'],
+    contexts: [
+      { name: 'BartStations', title: 'BART Stations'},
+      {name: 'BartRoutes', title: 'BART Routes'},
+      {name:'BartArrivals', title: 'BART Arrivals'}
+    ],
     currentContext: 'BartStations',
     collections: [
       {
@@ -403,12 +242,181 @@ var DataCardApplication = React.createClass({
       }
     ]
   },
-  getInitialState() {
+  moveCard: function (collectionName, direction) {
+    function adjustParentCard(collectionIx, myCase) {
+      if (collectionIx <= 0) {
+        return;
+      }
+      var parentId = myCase.parentId;
+      var parentCollection = collections[collectionIx - 1];
+      if (parentCollection.cases[parentCollection.currentCaseIndex].id === parentId) {
+        return;
+      }
+      var parentCaseIndex = parentCollection.cases.findIndex(function(iCase) {
+        return iCase.id === parentId;
+      });
+      console.log('adjustParentCard. ' + JSON.stringify({
+            parentId: parentId,
+            parentCollectionCaseIds: parentCollection.cases.map(function (iCase) {return iCase.id}),
+            collectionIx: collectionIx,
+            parentCaseIndex: parentCollection.currentCaseIndex
+          }));
+      if (parentCaseIndex >= 0) {
+        parentCollection.currentCaseIndex = parentCaseIndex;
+      }
+      adjustParentCard(collectionIx - 1, parentCollection.cases[parentCollection.currentCaseIndex])
+    }
+    function adjustChildCard(collectionIx, myCase){
+      if (collectionIx + 1 >= collections.length) {
+        return;
+      }
+      var parentId = myCase.id;
+      var childCollection = collections[collectionIx + 1];
+      var childCase  = childCollection.cases[childCollection.currentCaseIndex];
+      var childCaseIx;
+      if (childCase.parentId !== parentId) {
+        childCaseIx = childCollection.cases.findIndex(function (iCase) { return iCase.parentId === parentId; });
+        console.log('adjustChildCard. ' + JSON.stringify({
+              parentId: parentId,
+              childCollectionCaseIds: childCollection.cases.map(function (iCase) {return iCase.id}),
+              collectionIx: collectionIx,
+              childCaseIndex: childCaseIx
+            }));
+        if (childCaseIx >= 0) {
+          childCollection.currentCaseIndex = childCaseIx;
+        }
+        adjustChildCard(collectionIx + 1, childCollection.cases[childCollection.currentCaseIndex]);
+      }
+    }
+    var newCaseIx = null;
+    var collections = this.data.collections;
+    var collectionIx = collections.findIndex(function (collection) {
+      return collection.name === collectionName;
+    });
+    var collection = (collectionIx >= 0) && this.data.collections[collectionIx];
+
+    console.log('moveCard. ' + JSON.stringify({
+          collection: collectionName,
+          direction: direction,
+          collectionIx: collectionIx,
+          length: collection.cases.length,
+          currentCaseIndex: collection.currentCaseIndex
+        }));
+    if (collection) {
+      if (direction === 'left' && collection.currentCaseIndex > 0) {
+        newCaseIx = collection.currentCaseIndex = collection.currentCaseIndex - 1;
+      } else if (direction === 'right' && (collection.currentCaseIndex + 1) < collection.cases.length) {
+        newCaseIx = collection.currentCaseIndex = collection.currentCaseIndex + 1;
+      } else {
+        return;
+      }
+      adjustParentCard(collectionIx, collection.cases[newCaseIx]);
+      adjustChildCard(collectionIx, collection.cases[newCaseIx]);
+    }
+  },
+  getState: function () {
     return this.data;
+  }
+});
+
+var ContextMenu = React.createClass({
+  render: function () {
+    var options = this.props.contexts.map(function (context) {
+      return (
+          <option key={context.name} >{context.title}</option>
+      );
+    });
+    return <section id="context-selector">
+      <label>Context:&nbsp;
+        <select id="context-selector" >{options}</select>
+      </label >
+    </section>
+  }
+});
+
+var AttrList = React.createClass({
+  render: function () {
+    var items = this.props.attrs.map(function (item) {
+      return <div key={item.name} className="attr">{item.title}</div>
+    });
+    return <div className="attr-list">{items}</div>
+  }
+});
+
+var CaseDisplay = React.createClass ({
+  render: function () {
+    var myCase = this.props.myCase;
+    var values = this.props.attrs.map(function (attr) {
+      return <div className="attr-value" key={attr.name}> {myCase.values[attr.name]} </div>;
+    });
+    return <div className="case">{values}</div>;
+  }
+});
+
+var CaseList = React.createClass({
+  render: function () {
+    var caseIndex = this.props.collection.currentCaseIndex || 0;
+    var myCase = this.props.collection.cases[caseIndex];
+    var caseView = <CaseDisplay key={myCase.id} attrs={this.props.collection.attrs} myCase={myCase}/>;
+    return <div className="case-container"> {caseView} </div>;
+  }
+});
+
+var CaseNavControl = React.createClass({
+  symbol: {
+    left: '<',
+    right: '>'
+  },
+  handleClick(ev) {
+    this.props.onNavigation && this.props.onNavigation(this.props.action);
+  },
+  render: function () {
+    return <div className="control" onClick={this.handleClick}>{this.symbol[this.props.action]}</div>
+  }
+});
+
+var DataCard = React.createClass({
+  moveCard: function (direction) {
+    console.log('moveCard: direction: ' + direction);
+    this.props.onNavigation && this.props.onNavigation(this.props.collection.name, direction);
+  },
+  render: function () {
+    return <section className="card-section">
+      <div className="collection-name">{this.props.collection.title}</div>
+      <div className="card-deck">
+        <div className="left-ctls">
+          <CaseNavControl action="left" onNavigation={this.moveCard} />
+        </div>
+        <div className="attr-container">
+          <AttrList attrs={this.props.collection.attrs} />
+        </div>
+        <div className="case-frame">
+          <CaseList collection={this.props.collection} />
+        </div>
+        <div className="right-ctls">
+          <CaseNavControl action="right"  onNavigation={this.moveCard}/>
+          <div className="control ctl-add-case">+</div>
+        </div>
+      </div>
+    </section>
+  }
+});
+
+var DataCardApp = React.createClass({
+  navigate: function (collectionName, direction) {
+    dataManager.moveCard(collectionName, direction);
+    this.didChange();
+  },
+  didChange: function (state) {
+    this.setState(dataManager.getState());
+  },
+  getInitialState() {
+    return dataManager.getState();
   },
   render: function () {
     var cards = this.state.collections.map(function (collection){
-      return <DataCard key={collection.name} collection={collection} onNavigation={this.moveCard} />
+      return <DataCard key={collection.name} collection={collection}
+                       onNavigation={this.navigate} />
     }.bind(this));
     return <div>
       <ContextMenu contexts={this.state.contexts} />
@@ -417,5 +425,6 @@ var DataCardApplication = React.createClass({
   }
 });
 
-ReactDOM.render(<DataCardApplication />,
+ReactDOM.render(<DataCardApp data={dataManager} />,
     document.getElementById('container'));
+
