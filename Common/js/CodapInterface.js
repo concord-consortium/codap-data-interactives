f3e6b5681f87b0c8222f98d142ec2b8467c0ebc2
Jonathan Sandoe <jsandoe@concord.org>
2016-12-07 07:18:08 -0800

    Add Questionnaire data interactive.
    This Data Interactive allows a user to define a pair of surveys using
    the survey designer at http://surveyjs.com, then create a document
    that runs the surveys. The answers to the surveys are sent added to
    a data set in CODAP.

---
+++ Common/js/CodapInterface.js
@@ -0,0 +1,311 @@
+// ==========================================================================
+//  
+//  Author:   jsandoe
+//
+//  Copyright (c) 2016 by The Concord Consortium, Inc. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// ==========================================================================
+
+/**
+ * This class is intended to provide an abstraction layer for managing
+ * a CODAP Data Interactive's connection with CODAP. It is not required. It is
+ * certainly possible for a data interactive, for example, to use only the
+ * iFramePhone library, which manages the connection at a lower level.
+ *
+ * This object provides the following services:
+ *   1. Initiates the iFramePhone interaction with CODAP.
+ *   2. Provides information on the status of the connection.
+ *   3. Provides a sendRequest method. It accepts a callback or returns a Promise
+ *      for handling the results from CODAP.
+ *   4. Provides a subscriber interface to receive selected notifications from
+ *      CODAP.
+ *   5. Provides automatic handling of Data Interactive State. Prior to saving
+ *      a document CODAP requests state from the Data Interactive, where state
+ *      is an arbitrary serializable object containing whatever the data
+ *      interactive needs to retain. It returns this state when the document
+ *      is reopened.
+ *   6. Provides a utility to parse a resource selector into its component parts.
+ *
+ * @type {Object}
+ */
+var codapInterface = Object.create({
+  /**
+   * The CODAP Connection
+   * @param {iframePhone.IframePhoneRpcEndpoint}
+   */
+  connection: null,
+
+  /**
+   * Current known state of the connection
+   * @param {'preinit' || 'init' || 'active' || 'inactive' || 'closed'}
+   */
+  connectionState: 'preinit',
+
+  /**
+   * Connection statistics
+   */
+  stats: {
+    countDiReq: 0,
+    countDiRplSuccess: 0,
+    countDiRplFail: 0,
+    countDiRplTimeout: 0,
+    countCodapReq: 0,
+    countCodapUnhandledReq: 0,
+    countCodapRplSuccess: 0,
+    countCodapRplFail: 0,
+    timeDiFirstReq: null,
+    timeDiLastReq: null,
+    timeCodapFirstReq: null,
+    timeCodapLastReq: null
+  },
+
+  /**
+   * A serializable object shared with CODAP. This is saved as a part of the
+   * CODAP document. It is intended for the data interactive's use to store
+   * any information it may need to reestablish itself when a CODAP document
+   * is saved and restored.
+   *
+   * This object will be initially empty. It will be updated during the process
+   * initiated by the init method if CODAP was started from a previously saved
+   * document.
+   */
+  interactiveState: {},
+
+  /**
+   * A list of subscribers to messages from CODAP
+   * @param {[{actionSpec: {RegExp}, resourceSpec: {RegExp}, handler: {function}}]}
+   */
+  notificationSubscribers: [],
+
+  config: null,
+
+  /**
+   * Initialize connection.
+   *
+   * Start connection. Request interactiveFrame to get prior state, if any.
+   * Update interactive frame to set name and dimensions.
+   *
+   * @param config {object} Configuration. Optional properties: title {string},
+   *                        version {string}, dimensions {object}
+   *
+   * @param callback {function(interactiveState)}
+   * @return {Promise} Promise of interactiveState;
+   */
+  init: function (config, callback) {
+    return new Promise(function (resolve, reject) {
+      function getFrameRespHandler(resp) {
+          var success = resp && resp[1] && resp[1].success;
+          var receivedFrame = success && resp[1].values;
+          var savedState = receivedFrame && receivedFrame.savedState;
+          this_.updateInteractiveState(savedState);
+          if (callback) {
+            callback(savedState);
+          }
+          // deprecated way of conveying state
+          if (config.stateHandler) {
+            config.stateHandler(savedState);
+          }
+          resolve(savedState);
+      }
+
+      var getFrameReq = {action: 'get', resource: 'interactiveFrame'};
+      var newFrame = {
+        name: config.name,
+        title: config.title,
+        version: config.version,
+        dimensions: config.dimensions,
+        preventBringToFront: config.preventBringToFront
+      };
+      var updateFrameReq = {
+        action: 'update',
+        resource: 'interactiveFrame',
+        values: newFrame
+      };
+      var this_ = this;
+
+      this.config = config;
+
+      // initialize connection
+      this.connection = new iframePhone.IframePhoneRpcEndpoint(
+          this._notificationHandler.bind(this), "data-interactive", window.parent);
+
+      this.on('get', 'interactiveState', function () {
+        console.log('sending interactiveState: ' + JSON.stringify(this.interactiveState));
+        return ({success: true, values: this.getInteractiveState()});
+      }.bind(this));
+
+      // update, then get the interactiveFrame.
+      this.sendRequest([updateFrameReq, getFrameReq]).then(getFrameRespHandler);
+    }.bind(this));
+  },
+
+  getStats: function () {
+    return this.stats;
+  },
+
+  /**
+   * Returns the interactive state.
+   *
+   * @returns {object}
+   */
+  getInteractiveState: function () {
+    return this.interactiveState;
+  },
+
+  /**
+   * Updates the interactive state.
+   * @param interactiveState
+   */
+  updateInteractiveState: function (interactiveState) {
+    if (!interactiveState) {
+      return;
+    }
+    this.interactiveState = Object.assign(this.interactiveState, interactiveState);
+
+  },
+
+  destroy: function () {},
+
+  /**
+   * Sends a request to CODAP. The format of the message is as defined in
+   * https://github.com/concord-consortium/codap/wiki/CODAP-Data-Interactive-API.
+   *
+   * @param message
+   * @param callback {function(response, request)} Optional callback to handle
+   *    the CODAP response. Note both the response and the initial request will
+   *    sent.
+   *
+   * @return {Promise} The promise of the response from CODAP.
+   */
+  sendRequest: function (message, callback) {
+    return new Promise(function (resolve, reject){
+      function handleResponse (request, response, callback) {
+        if (response === undefined) {
+          console.warn('handleResponse: CODAP request timed out');
+          reject('handleResponse: CODAP request timed out: ' + JSON.stringify(request));
+          this_.stats.countDiRplTimeout++;
+        } else {
+          this_.connectionState = 'active';
+          response.success? this_.stats.countDiRplSuccess++: this_.stats.countDiRplFail++;
+          resolve(response);
+        }
+        if (callback) {
+          callback(response, request);
+        }
+      }
+      var this_ = this;
+      switch (this.connectionState) {
+        case 'closed': // log the message and ignore
+          console.warn('sendRequest on closed CODAP connection: ' + JSON.stringify(message));
+          reject('sendRequest on closed CODAP connection: ' + JSON.stringify(message));
+          break;
+        case 'preinit': // warn, but issue request.
+          console.warn('sendRequest on not yet initialized CODAP connection: '
+              + JSON.stringify(message));
+          // fallthrough intentional
+        default:
+          if (this.connection) {
+            this.stats.countDiReq++;
+            this.stats.timeDiLastReq = new Date();
+            if (!this.stats.timeDiFirstReq) {
+              this.stats.timeCodapFirstReq = this.stats.timeDiLastReq;
+            }
+
+            this.connection.call(message, function (response) {
+              handleResponse(message, response, callback)
+            });
+          } else {
+            console.error('sendRequest on non-existent CODAP connection');
+          }
+      }
+    }.bind(this));
+  },
+
+  _notificationHandler: function (request, callback) {
+    var action = request.action;
+    var resource = request.resource;
+    var stats = this.stats;
+
+    this.connectionState = 'active';
+    stats.countCodapReq++;
+    stats.timeCodapLastReq = new Date();
+    if (!stats.timeCodapFirstReq) {
+      stats.timeCodapFirstReq = stats.timeCodapLastReq;
+    }
+
+    var handled = this.notificationSubscribers.some(function (subscription) {
+      if (subscription.actionSpec.test(action)
+          && subscription.resourceSpec.test(resource)) {
+        var rtn = subscription.handler(request);
+        if (rtn && rtn.success) {
+          stats.countCodapRplSuccess++;
+        } else {
+          stats.countCodapRplFail++;
+        }
+        callback(rtn);
+        return true;
+      }
+      return false;
+    });
+    if (!handled) {
+      stats.countCodapUnhandledReq++;
+      callback({success: true});
+    }
+  },
+
+  /**
+   * Registers a handler to respond to CODAP-initiated requests and
+   * notifications.
+   *
+   * @param actionSpec {regex} A regular expression to qualify actions.
+   * @param resourceSpec {regex} A regular expression to qualify resources.
+   * @param handler
+   */
+  on: function (actionSpec, resourceSpec, handler) {
+    this.notificationSubscribers.push({
+      actionSpec: RegExp(actionSpec),
+      resourceSpec: RegExp(resourceSpec),
+      handler: handler
+    });
+  },
+
+  /**
+   * Parses a resource selector returning a hash of named resource names to
+   * resource values. The last clause is identified as the resource type.
+   * E.g. converts 'dataContext[abc].collection[def].case'
+   * to {dataContext: 'abc', collection: 'def', type: 'case'}
+   * @param iResource
+   * @returns {{}}
+   */
+  parseResourceSelector: function (iResource) {
+    var selectorRE = /([A-Za-z0-9_-]+)\[([^\]]+)]/;
+    var result = {};
+    var selectors = iResource.split('.');
+    selectors.forEach(function (selector) {
+      var resourceType, resourceName;
+      var match = selectorRE.exec(selector);
+      if (selectorRE.test(selector)) {
+        resourceType = match[1];
+        resourceName = match[2];
+        result[resourceType] = resourceName;
+        result.type = resourceType;
+      } else {
+        result.type = selector;
+      }
+    });
+
+    return result;
+  },
+
+});
---
+++ questionnaire/js/questionnaire.js
@@ -0,0 +1,340 @@
+/*
+ * Manages a questionnaire dialog. Sends the questionnaire results to CODAP.
+ */
+/* global Survey*/
+
+/**
+ * Helper to connect to CODAP.
+ * @type {Object}
+ */
+
+var kStartupSurvey = {
+  "completedHtml": '<button class="restart-setup">Redo setup</button> ' +
+      '<button class="start-up-complete">Begin</button>',
+  "pages": [
+    {
+      "name": "page1",
+      "questions": [
+        {
+          "type": "html",
+          "html": "This is a questionnaire plugin to " +
+              "<a href=\"http://codap.concord.org\">CODAP</a>. It is built on " +
+              "<a href=\"http://surveyjs.org/\">Survey.js</a>. You can design " +
+              "surveys using the design tool found " +
+              "<a href=\"http://surveyjs.org/builder/\">here</a>. Be sure to " +
+              "select the option \"Generate Valid JSON\". When you have designed " +
+              "a survey, click on the \"JSON Editor\" tab. Select the JSON " +
+              "text, copy it and paste it into a text document.  The " +
+              "questionnaire is designed to have a data set with two levels, " +
+              "the first level describes the experimental conditions and the " +
+              "second level is for recording observations. Each level requires its own survey. There can be many " +
+              "observations for each experiment.",
+          "name": "question1"
+        },
+        {
+          "type": "dropdown",
+          "choices": [
+            "one",
+            {
+              "value": "two",
+              "text": "second value"
+            },
+            {
+              "value": "three",
+              "text": "third value"
+            }
+          ],
+          "choicesOrder": "asc",
+          "hasOther": true,
+          "name": "dataSetName",
+          "otherErrorText": "Name of new data set",
+          "otherText": "New Data Set",
+          "title": "Data Set"
+        },
+        {
+          "type": "file",
+          "name": "survey1",
+          "title": "Survey file (level 1)",
+          "storeDataAsText": true
+        },
+        {
+          "type": "file",
+          "name": "survey2",
+          "title": "Survey file (level 2)",
+          "storeDataAsText": true
+        }
+      ]
+    }
+  ]
+};
+
+var kSurvey1CompletionHTML = '<button class="survey-1-complete">Enter Observations</button>';
+
+var kSurvey2CompletionHTML = '<button class="survey-2-complete">Enter Another Observation</button>' +
+    '<button class="survey-2-complete-restart">Start over</button>';
+
+var contextDef;
+
+var survey; // will contain a reference to the currently running survey
+var surveyDocs = {}; // will contain specifications for the two surveys we
+                     // will be using.
+var myState = null; // current state of the DI form data so far
+
+var itemData = {}; // In progress item data acquired so far.
+
+
+function inferContextFromSurveys(surveys, contextName) {
+  var collections = [];
+  var lastCollectionName;
+  ['survey1', 'survey2'].forEach(function (surveyName) {
+    var survey = surveys[surveyName];
+    if (!survey) {
+      return;
+    }
+    var attributes = [];
+    var collection = {name: survey.title, attrs: attributes};
+    collection.parent = lastCollectionName;
+    lastCollectionName = survey.title;
+    survey.pages.forEach(function (page) {
+      page.questions.forEach(function (question) {
+        attributes.push({name: question.name});
+      });
+    });
+    collections.push(collection);
+  });
+  return {name: contextName, collections: collections};
+}
+
+function getSurveyAttributeNames(surveys) {
+  var attrs = [];
+  surveys.forEach(function (survey) {
+    survey && survey.pages && survey.pages.forEach(function (page) {
+      page.questions && page.questions.forEach(function (question) {
+        attrs.push(question.name);
+      })
+    })
+  })
+  return attrs;
+}
+
+function getDataContextAttributeNames(dataContext) {
+  if (!dataContext) {
+    return;
+  }
+  var names = [];
+  dataContext.collections.forEach(function (collection) {
+    collection.attrs.forEach(function (attr) {
+      names.push(attr.name);
+    });
+  });
+  return names;
+}
+
+function findMissingAttributes(surveys, dataContextDef) {
+  var surveyAttrNames = getSurveyAttributeNames(surveys).sort();
+  var dataContextAttrNames = getDataContextAttributeNames(dataContextDef).sort();
+  var onlyInSurvey = [];
+  surveyAttrNames && surveyAttrNames.forEach(function (name) {
+    if (dataContextAttrNames.indexOf(name) < 0) {
+      onlyInSurvey.push(name);
+    }
+  });
+  return onlyInSurvey;
+}
+/**
+ *
+ * @returns {*|Promise} Promise of an array of missing attributes, if any.
+ */
+function prepareDataContext() {
+// Request expected data context, if not found, create it
+  return codapInterface.sendRequest({action: 'get', resource: 'dataContext[' + myState.dataSetName  + ']'})
+      .then(function (reply) {
+        if (reply && !reply.success) {
+          contextDef = inferContextFromSurveys(myState, myState.dataSetName );
+          codapInterface.sendRequest({action:'create', resource: 'dataContext', values: contextDef});
+          return Promise.resolve([])
+        } else {
+          return Promise.resolve(findMissingAttributes([myState.survey1, myState.survey2], reply.values));
+        }
+      });
+}
+
+function addSurvey(surveyDoc, onCompleteHandler) {
+  $('#messages').empty();
+  Survey.Survey.cssType = "standard";
+  survey = new Survey.Survey(surveyDoc);
+
+  survey.onComplete.add(function (formData) {
+    onCompleteHandler(formData);
+  });
+
+  survey.completeText = "Next";
+  survey.render("surveyAttachPoint");
+}
+
+function removeSurvey() {
+   $('#surveyAttachPoint>*').remove();
+}
+
+$('body').on('click', '.survey-1-complete', function () {
+  removeSurvey();
+  addSurvey(myState.survey2, survey2CompletionHandler);
+});
+
+$('body').on('click', '.survey-2-complete', function () {
+  removeSurvey();
+  addSurvey(myState.survey2, survey2CompletionHandler);
+});
+
+$('body').on('click', '.survey-2-complete-restart', function () {
+  removeSurvey();
+  addSurvey(myState.survey1, survey1CompletionHandler);
+});
+
+$('body').on('click', '.start-up-complete', function () {
+  removeSurvey();
+  addSurvey(myState.survey1, survey1CompletionHandler);
+});
+
+$('body').on('click', '.restart-setup', function () {
+  removeSurvey();
+  addSurvey(kStartupSurvey, startupSurveyCompletionHandler);
+});
+
+
+function survey1CompletionHandler(formData) {
+  itemData = formData.data;
+}
+
+function survey2CompletionHandler(formData) {
+  itemData = Object.assign(itemData, formData.data);
+  codapInterface.sendRequest({action: 'create',
+        resource: 'dataContext[' + myState.dataSetName  + '].item', values: itemData})
+    .then(function (result) {
+      if (result && result.success) {
+        if (result.values && result.values.length > 0) {
+          return codapInterface.sendRequest({action:'create',
+            resource: 'dataContext[' + myState.dataSetName  + '].selectionList',
+            values: result.values})
+        }
+      }
+    });
+  console.log("The results are:" + JSON.stringify(formData.data));
+}
+
+function startupSurveyCompletionHandler(data) {
+  if (!data) {
+    return;
+  }
+  if (!myState) {
+    myState = codapInterface.getInteractiveState();
+  }
+  var answers = data.data;
+  var surveyAttributes;
+
+  // Apply name of data set. If new, we have to find it in a different field.
+  if (answers.dataSetName === 'other') {
+    myState.dataSetName = answers['dataSetName-Comment'];
+  } else {
+    myState.dataSetName = answers.dataSetName;
+  }
+  // survey.js doesn't give us direct access to the survey file. Instead it creates
+  // a data uri with the file information. Here, we base64 decode the dataURI, then
+  // attempt to parse.
+  myState.survey1 = convertDataURIToJSON(answers.survey1);
+  myState.survey1.completedHtml = kSurvey1CompletionHTML;
+  myState.survey2 = convertDataURIToJSON(answers.survey2);
+  myState.survey2.completedHtml = kSurvey2CompletionHTML;
+  //surveyAttributes = getSurveyAttributes([myState.survey1, myState.survey2]);
+  prepareDataContext()
+    .then(function (reply) {
+      // addSurvey(myState.survey1, survey1CompletionHandler);
+      if (reply.length > 0) {
+        $("#messages").html('<span class="error">Warning:</span> the following ' +
+            'items are defined in the questionnaire, but not known to CODAP, so ' +
+            'will not be submitted: ' + reply.join());
+      } else {
+        $("#messages").text('At any time after this point you can save and/or share ' +
+            'this document. When the document is reopened it will show the ' +
+            'questionnaire dialog and not the foregoing setup dialog.');
+      }
+
+    });
+}
+
+// function loadSurvey1() {
+//   return loadSurvey('survey1', myState.surveyURL1,
+//       '<button class="survey-1-complete">Enter Observations</button>',
+//       survey1CompletionHandler);
+// }
+
+// function loadSurvey2() {
+//   return loadSurvey('survey2', myState.surveyURL2,
+//       '<button class="survey-2-complete">Enter Another Observation</button>' +
+//       '<button class="survey-2-complete-restart">Start over</button>',
+//       survey2CompletionHandler);
+// }
+
+/**
+ * Utility to attempt convert DataURI to JSON.
+ *
+ * If conversion fails, will return null and log to console.
+ *
+ * @param dataURI
+ */
+function convertDataURIToJSON(dataURI) {
+  dataUriRE = /^data:[^;]*;base64,/
+  if (!dataURI) {
+    return;
+  }
+  if (dataUriRE.test(dataURI)) {
+    var x = atob(dataURI.replace(dataUriRE, ''));
+    try {
+      return JSON.parse(x);
+    } catch (ex) {
+      console.warn('Error parsing json file: ' + ex);
+    }
+  } else {
+    console.warn('Attempted to convert file that does not appear to be a json file.');
+  }
+}
+
+// update data set names in survey based on list returned by CODAP
+function updateDataSetList(survey) {
+  // locate the data set question in the survey
+  var dataSetQuestion = survey.pages[0].questions.find(function(question) { return question.name === 'dataSetName';});
+
+  return codapInterface.sendRequest({action: 'get', resource: 'dataContextList'})
+    .then(function (data) {
+        if (data) {
+          var choices = data.values.map(function (dataContext) {return dataContext.name;})
+          dataSetQuestion.choices = choices;
+        }
+      }
+    );
+}
+
+// Start CODAP connection
+codapInterface.init({name: "Questionnaire",
+      title: "Questionnaire",
+      version: "0.1",
+      dimensions: {width:480, height: 640},
+      preventBringToFront: false,
+    })
+  // then, determine whether we are starting fresh or have already loaded
+  // data-acquisition surveys. If the later, start the survey, if the former
+  // start the startup dialog.
+  .then(function (interactiveState) {
+    if (interactiveState && interactiveState.survey1) {
+      myState = codapInterface.getInteractiveState();
+      prepareDataContext().then(function () {
+        addSurvey(myState.survey1, survey1CompletionHandler);
+      })
+    } else {
+      updateDataSetList(kStartupSurvey)
+        .then(function () {
+          addSurvey(kStartupSurvey, startupSurveyCompletionHandler);
+        });
+    }
+  })
+  .catch( function (msg) { console.warn(msg)});
