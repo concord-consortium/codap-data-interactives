ce9b44995b2572fd0d6079c3673d48ef5b228621
Sam Fentress <sfentress@concord.org>
2017-02-06 14:30:02 -0500

    Add simple addition/removal of variables
    Simple scaling, keeping them all at the bottom for now.

--- TP-Sampler/src/app.js
+++ TP-Sampler/src/app.js
@@ -17,7 +17,9 @@ function createMixer() {
 }
 
 function addMixerVariables() {
-  var radius = 15;
+  var minRadius = 15,
+      maxRadius = (width - border * 2) / (variables.length * 2),
+      radius = Math.min(minRadius, maxRadius);
   // other calcs...
   for (var i = 0, ii=variables.length; i<ii; i++) {
     var x = border + radius + (i * radius * 2),
@@ -30,11 +32,31 @@ function addMixerVariables() {
           strokeWidth: 1
       }),
       s.text(x, y, variables[i]).attr({
+        fontSize: radius,
         textAnchor: "middle",
         dy: ".25em"
       })
     ));
   }
 }
+
+function getNextVariable() {
+  // brain-dead version for now
+  return "" + (variables.length + 1);
+}
+
+function addVariable() {
+  variables.push(getNextVariable());
+  createMixer();
+}
+
+function removeVariable() {
+  variables.pop();
+  createMixer();
+}
+
+document.getElementById("add-variable").onclick = addVariable;
+document.getElementById("remove-variable").onclick = removeVariable;
+
 
 createMixer();
