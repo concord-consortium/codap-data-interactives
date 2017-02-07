1dd80b3e5dc9f1e1943a23c225c3a6629948e25a
Sam Fentress <sfentress@concord.org>
2017-02-07 13:45:30 -0500

    Connect to CODAP, create cases as they are made

--- TP-Sampler/src/app.js
+++ TP-Sampler/src/app.js
@@ -2,6 +2,15 @@ var s = Snap("#model svg"),
     width = 200,      // svg units
     height = 100,
     border = 2,
+    experimentNumber = 0,
+    runNumber = 0,
+
+    numRuns = 5,
+    sampleSize = 2,
+
+    experimentCaseID,
+    runCaseID,
+
     variables = ["a", "b", "a"],
     balls = [];
 
@@ -87,36 +96,45 @@ function getTransformForMovingTo(x, y, circ) {
 }
 
 function run() {
-  var sequence = createRandomSequence(20, 1);
+  startNewExperimentInCODAP().then(function() {
+    var sequence = createRandomSequence(sampleSize, numRuns);
 
-  // selection animation
-  function select(i) {
-    var ball = balls[i],
-        circle = ball.select("circle"),
-        trans = getTransformForMovingTo(100, circle.attr("r") * 1, circle);
-    ball.animate({transform: trans}, 500, function() {
-      ball.animate({transform: "T0,0"}, 500);
-    });
-  }
-
-  var run = 0,
-      draw = 0;
-
-  function selectNext() {
-    select(sequence[run][draw]);
-    if (draw < sequence[run].length - 1) {
-      draw++;
-    } else {
-      run++;
-      draw = 0;
+    // selection animation
+    function select(run, draw, selectionMadeCallback) {
+      var selection = sequence[run][draw],
+          ball = balls[selection],
+          circle = ball.select("circle"),
+          trans = getTransformForMovingTo(100, circle.attr("r") * 1, circle);
+      ball.animate({transform: trans}, 500, function() {
+        selectionMadeCallback(draw, variables[selection]);
+        ball.animate({transform: "T0,0"}, 500);
+      });
     }
-    if (sequence[run]) {
+
+    function scheduleNextSelection() {
+      // for now just assume every 1000ms
       setTimeout(selectNext, 1000);
     }
-  }
 
-  selectNext();
+    var run = 0,
+        draw = 0;
 
+    function selectNext() {
+      select(run, draw, addValueToCODAP);
+
+      if (draw < sequence[run].length - 1) {
+        draw++;
+      } else {
+        run++;
+        draw = 0;
+      }
+      if (sequence[run]) {
+        scheduleNextSelection();
+      }
+    }
+
+    selectNext();
+  });
 }
 
 document.getElementById("add-variable").onclick = addVariable;
@@ -125,3 +143,142 @@ document.getElementById("run").onclick = run;
 
 
 createMixer();
+
+// connect to CODAP
+
+codapInterface.on('get', 'interactiveState', function () {
+  return {success: true, values: {}};
+});
+
+// initialize the codapInterface
+codapInterface.init({
+    name: 'Sampler',
+    title: 'Sampler',
+    dimensions: {width: 350, height: 250},
+    version: '0.1',
+    stateHandler: function (state) {
+      console.log('stateHandler: ' + (state && JSON.stringify(state)));
+    }
+  }).then(function () {
+    // Determine if CODAP already has the Data Context we need.
+    // If not, create it.
+    return codapInterface.sendRequest({
+        action:'get',
+        resource: 'dataContext[Sampler]'
+      }, function (result) {
+        if (result && !result.success) {
+          codapInterface.sendRequest({
+            action: 'create',
+            resource: 'dataContext',
+            values: {
+              name: "Sampler",
+              collections: [
+                {
+                  name: 'experiments',
+                  attrs: [
+                    {name: "experiment", type: 'categorical'},
+                    {name: "draws", type: 'categorical'}
+                  ],
+                  childAttrName: "experiment"
+                },
+                {
+                  name: 'runs',
+                  parent: 'experiments',
+                  // The parent collection has just one attribute
+                  attrs: [{name: "run", type: 'categorical'}],
+                  childAttrName: "run"
+                },
+                {
+                  name: 'draws',
+                  parent: 'runs',
+                  labels: {
+                    pluralCase: "draws",
+                    setOfCasesWithArticle: "a draw"
+                  },
+                  // The child collection also has just one attribute
+                  attrs: [{name: "value"}]
+                }
+              ]
+            }
+          }, function(result) { console.log(result)});;
+        }
+      }
+    );
+  });
+
+function startNewExperimentInCODAP() {
+  return new Promise(function(resolve, reject) {
+    if (!codapInterface.connection.isConnected()) {
+      console.log('Not in CODAP')
+      resolve();
+      return;
+    }
+
+    experimentNumber++;
+    runNumber = 0; // Each experiment starts the runNumber afresh
+    codapInterface.sendRequest({
+      action: 'create',
+      resource: 'collection[experiments].case',
+      values: [{
+        values: {
+          experiment: experimentNumber,
+          draws: sampleSize
+        }
+      }]
+    }, function (result) {
+      if (result.success) {
+        experimentCaseID = result.values[0].id;
+        resolve();
+      }
+      else {
+        window.alert('Unable to begin experiment');
+        reject();
+      }
+    });
+  });
+}
+
+function startNewRunInCODAP() {
+  if (!codapInterface.connection.isConnected()) {
+    resolve();
+    return;
+  }
+
+  runNumber++;
+  return new Promise(function(resolve) {
+    codapInterface.sendRequest({
+      action: 'create',
+      resource: 'collection[runs].case',
+      values: {
+        parent: experimentCaseID,
+        values: {run: runNumber}
+      }
+    }, function(result) {
+      runCaseID = result.values[0].id;
+      resolve()
+    });
+  });
+}
+
+function addValueToCODAP(draw, val) {
+  if (!codapInterface.connection.isConnected()) {
+    return;
+  }
+
+
+  function _addValue() {
+    codapInterface.sendRequest({
+      action: 'create',
+      resource: 'collection[draws].case',
+      values: {
+        parent: runCaseID,
+        values: {value: val}
+      }
+    });
+  }
+  if (draw === 0) {
+    startNewRunInCODAP().then(_addValue);
+  } else {
+    _addValue();
+  }
+}
