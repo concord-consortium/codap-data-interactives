1c8fe4d6cf7916f53c19b3e3c7685c3952dcd5b5
Miguel Gutierrez <miguelgutz95@gmail.com>
2017-06-19 13:36:53 -0700

    added automatic documentation comments

--- Charts/src/charts_controller.js
+++ Charts/src/charts_controller.js
@@ -1,30 +1,75 @@
+//=================================================================
+//
+//   Author: Miguel Gutierrez
+//   Date: June 2017
+//
+//  Copyright (c) 2016 by The Concord Consortium, Inc. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//=================================================================
+/**
+ * @constructor ChartController - this object communicates with the model object
+ *              and the view object in order to update the data and the UI
+ * @param  {Object} model - tracks the data in CODAP
+ * @param  {Object} view - tracks the data the user sees
+ */
 var ChartController = function(model, view){
   this.model = model;
   this.view = view;
-
-
   this.init();
 };
+
 ChartController.prototype = {
+  /**
+   * @function initializes the object
+   */
   init: function(){
     this.createChildren()
     .setupHandlers()
     .enable()
     .codapListeners();
   },
+  /**
+   * creates the objects the class needs. In this case it doesn't need any
+   * @return this for chainning
+   */
   createChildren: function(){
     return this;
   },
+  /**
+   * @function binds the handlers with the functions
+   * @return this
+   */
   setupHandlers: function(){
     this.contextHandler = this.contextCountChanged.bind(this);
     return this;
   },
+  /**
+   * @function adds handler functions to the event listeners that are triggered by the user
+   * @return this
+   */
   enable: function(){
     return this;
   },
+  /**
+   * These are the CODAP listeners that are not specific to any data context
+   */
   codapListeners: function(){
     codapInterface.on('documentChangeNotice', 'dataContextCountChanged', this.contextHandler);
   },
+  /**
+   * These are the handler functions
+   */
   contextCountChanged: function(){
     this.model.updateDataContextList();
   }
--- Charts/src/charts_model.js
+++ Charts/src/charts_model.js
@@ -17,6 +17,15 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //=================================================================
+
+/**
+ *  @constructor ChartModel - creates a model for the data that needs to be
+ *     requested from CODAP.
+ *     It talks to CODAP in order to keep the user data updated based on
+ *     the user interactions during their session.
+ *     It tracks the user selections to have an interactive state.
+ *
+ */
 var ChartModel = function(){
   this.contextList = [];
   this.selected = {
@@ -25,45 +34,75 @@ var ChartModel = function(){
     chartType: null
   };
   this.changeContextCountEvent = new Event(this);
+  this.addAttribute = new Event(this);
+  this.moveAttribute = new Event(this);
 };
 ChartModel.prototype = {
+  /**
+   * @function updateDataContexList - requests context list from CODAP
+   *           in order to add new ones and notifies its event listener
+   */
   updateDataContextList: function(){
         getData().then((newContextList) => {
           for (var i = 0; i < newContextList.length; i++) {
-            if( !this.hasContext(newContextList[i]) ){
-              this.contextList.push( new Context(newContextList[i].name, newContextList[i].title, newContextList[i].id));
-              this.changeContextCountEvent.notify(
-                {name: newContextList[i].name}
-              );
-            }
+            if( !this.hasContext(newContextList[i].id) )
+              this.addNewContext(newContextList[i].name, newContextList[i].title, newContextList[i].id);
           }
         });
   },
-  hasContext: function(context){
+  /**
+   * @function addNewContext - adds context to the list and notifies event listeners
+   * @param  {string} name
+   * @param  {title} title
+   * @param  {id} id
+   */
+  addNewContext: function(name, title, id){
+    this.contextList.push(
+      new Context(name, title, id)
+    );
+    this.changeContextCountEvent.notify(
+      {name: newContextList[i].name}
+    );
+  }
+  /** @function hasContext
+  *   @param {number} context_id
+  *   @return {boolean}
+  */
+  hasContext: function(context_id){
     for(i = 0; i < this.contextList.length; i++){
-      if(this.contextList[i].id == context.id){
+      if(this.contextList[i].id == context_id){
         return true;
       }
     }
     return false;
   }
 };
-//This is the attribute object
-var Attribute = function(name, id, collection, color){
+
+/**
+ * @constructor This creates an attribute object in order to track
+ *              its collection and properties
+ * @param {string} name
+ * @param {number} id
+ * @param {string} collection
+ * @param {number[]} rgba_color
+ */
+var Attribute = function(name, id, collection, rgba_color){
   this.name = name || '';
   this.id = id || -1;
   this.collection = collection || '';
-  this.color = color || 'white';
+  this.color = rgba_color || 'white';
 };
-//this saves basic information for the context
-// the functions will allow access to information about
-// its attributes
+/**
+ * @constructor This creates a context objects in order to track
+ *              collections and attributes
+ * @param {string} name
+ * @param {string} title
+ * @param {number} id
+ */
 var Context = function(name, title, id){
   this.name = name || '';
   this.title = title || '';
   this.id = id || '';
   this.collectionList = [];
-  this.totalCollections = 0;
   this.attributeList = [];
-  this.totalAttributes = 0;
 };
