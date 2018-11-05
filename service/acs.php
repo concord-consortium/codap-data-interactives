be8778ca1f16aaed6e97f4c8d014e22b0159bc38
Jonathan Sandoe <jsandoe@concord.org>
2018-11-05 12:12:13 -0800

    Basic query response with new-style queries.

--- service/acs.php
+++ service/acs.php
@@ -47,11 +47,50 @@ header('Access-Control-Allow-Origin: *');
 function reportToFile($message)
 {
     file_put_contents("/tmp/acsdebug.txt", $message . "\n", FILE_APPEND);
+}
 
+function prefetch($DBH) {
+    $createTempQ = "CREATE TEMPORARY TABLE rand_numbers (number FLOAT)";
+
+    CODAP_MySQL_doQueryWithoutResult($DBH, $createTempQ, array());
+}
+
+function fetchSamples($DBH, $num, $yr, $st) {
+    $populateTempQ = "CALL InsertRandom(?, 0, 1)";
+    $populateTempParams = [$num];
+
+    $sumWeightsQ = "SELECT @sum_weights:=sum_weights FROM stats WHERE year=? AND state_code=?";
+    $selectSamplesYrStQ = "SELECT peeps.id,peeps.sample_data " .
+        "FROM peeps, rand_numbers WHERE peeps.year=? AND peeps.state_code=? AND " .
+        "(rand_numbers.number * @sum_weights) BETWEEN peeps.accum_yr_st AND " .
+        "peeps.accum_yr_st + peeps.perwt";
+    $selectSamplesYrQ = "SELECT peeps.id,peeps.sample_data " .
+        "FROM peeps, rand_numbers WHERE peeps.year=? AND " .
+        "(rand_numbers.number * @sum_weights) BETWEEN peeps.accum_yr AND " .
+        "peeps.accum_yr + peeps.perwt";
+    $params = [$yr, $st];
+
+    $depopulateTempQ = "DELETE FROM rand_numbers";
+
+    CODAP_MySQL_doQueryWithoutResult($DBH, $populateTempQ, $populateTempParams);
+    CODAP_MySQL_doQueryWithoutResult($DBH, $sumWeightsQ, $params);
+    if ($st) {
+        $rslt = CODAP_MySQL_getQueryResult($DBH, $selectSamplesYrStQ, $params);
+    } else {
+        $rslt = CODAP_MySQL_getQueryResult($DBH, $selectSamplesYrQ, $params);
+    }
+    CODAP_MySQL_doQueryWithoutResult($DBH, $depopulateTempQ, $populateTempParams);
+
+    return $rslt;
+}
+
+function postFetch ($DBH) {
+    $emptyTempQ = "DROP TABLE rand_numbers";
+    CODAP_MySQL_doQueryWithoutResult($DBH, $emptyTempQ, array());
 }
 
 
-//  ------------    Connect to database ------------
+//  -----------)-    Connect to database ------------
 
 
 include './TE_DBCommon.php';
@@ -61,6 +100,7 @@ include './TE_DBCommon.php';
 
 $whence  = $_REQUEST['whence'];
 
+// todo: derive from file
 $user = "root";
 $pass = "FsysHs,rd";
 $dbname = "sdlc";
@@ -68,13 +108,12 @@ $dbname = "sdlc";
 //$pass = $credentials[$whence]["pass"];
 //$dbname = $credentials[$whence]["dbname"];
 
-  reportToFile('Creds : ' . print_r($credentials, true) . " REQ: " . print_r($_REQUEST, true));
-  reportToFile("CRED TEST: whence \"$whence\" user \"$user\" pass \"$pass\" dbname \"$dbname\"");
+//  reportToFile('Creds : ' . print_r($credentials, true) . " REQ: " . print_r($_REQUEST, true));
+//  reportToFile("CRED TEST: whence \"$whence\" user \"$user\" pass \"$pass\" dbname \"$dbname\"");
 
 $DBH = CODAP_MySQL_connect("localhost", $user, $pass, $dbname);     //  works under MAMP....
 
 $params = array();  //  accumulate parameters for query
-$query = "SELECT * FROM peeps LIMIT 10";
 
 $command = $_REQUEST["c"];     //  this is the overall command, the only required part of the POST
 
@@ -83,27 +122,45 @@ $out = "{ Unhandled command : " . $command . "}";
 switch ($command) {
 
     case 'getCases':
-        reportToFile("[$command]............" . date("Y-m-d H:i:s (T)") . " cases: ".$_REQUEST['n']);
+        reportToFile("[$command]............" . date("Y-m-d H:i:s (T)") . " cases: " . $_REQUEST['n']);
+
         $params = array();
-        $params["n"] = $_REQUEST["n"];
-        $state_codes = $_REQUEST["state_codes"];
-        $years = $_REQUEST["years"];
+
+        $total_requested = $_REQUEST["n"];
+        assert($total_requested>0, 'requested samples missing or invalid');
+        $params["n"] = $total_requested;
+
+        $state_codes_raw = (isset($_REQUEST["state_codes"]))? $_REQUEST["state_codes"]: "" ;
+        $state_codes=explode(",", $state_codes_raw);
+        $state_codes=[30];
+        assert(is_array($state_codes), 'state codes invalid');
+
+        $years_raw = (isset($_REQUEST["years"]))?$_REQUEST["years"]: "";
+        $years = explode(",", $years_raw);
+        $years = [2000];
+        assert(is_array($years), 'sample years invalid');
 
 //        $theVariables = $_REQUEST['atts'];  //  includes opening comma
 
-        $query = "SELECT * FROM peeps ORDER BY RAND( ) LIMIT :n";
+        // determine per state/year request size
+        // determine if national or per state query
 
-        $out = CODAP_MySQL_getQueryResult($DBH, $query, $params);
+//        $query = "SELECT * FROM peeps ORDER BY RAND( ) LIMIT :n";
+        preFetch($DBH);
+        $out = fetchSamples($DBH, $total_requested, 2000, 30);
+        postFetch($DBH);
+
+//        $out = CODAP_MySQL_getQueryResult($DBH, $query, $params);
         //  reportToFile(".....[$command].(end)." . date("Y-m-d H:i:s (T)") . print_r($out, true));
         break;
 
-    case 'getAllAttributeInfo':
-        reportToFile("[$command]......." . date("Y-m-d H:i:s (T)"));
-        $params = array();
-
-        $query = "SELECT * FROM variables WHERE groupNumber < 50 ORDER By groupNumber";
-        $out = CODAP_MySQL_getQueryResult($DBH, $query, $params);
-        break;
+//    case 'getAllAttributeInfo':
+//        reportToFile("[$command]......." . date("Y-m-d H:i:s (T)"));
+//        $params = array();
+//
+//        $query = "SELECT * FROM variables WHERE groupNumber < 50 ORDER By groupNumber";
+//        $out = CODAP_MySQL_getQueryResult($DBH, $query, $params);
+//        break;
 
     case 'getYears':
         reportToFile("[$command]......." . date("Y-m-d H:i:s (T)"));
@@ -121,29 +178,29 @@ switch ($command) {
 
         break;
 
-    case 'getDecoderInfo':
-        reportToFile("[$command]......." . date("Y-m-d H:i:s (T)"));
-        $params = array();
-
-        $query = "SELECT DISTINCT varname FROM decoder";
-        $theVars = CODAP_MySQL_getQueryResult($DBH, $query, $params);
-        $out = array();
-
-        foreach ($theVars as $arrayOfValues) {
-            //  reportToFile('get Decoder -- looking at array: ' . print_r($arrayOfValues, true));
-            $var = $arrayOfValues[varname];
-            //  reportToFile('get Decoder -- looking at var: ' . print_r($var, true));
-            $query = "SELECT * FROM decoder WHERE varname = '".$var . "'";
-            $theCodes = CODAP_MySQL_getQueryResult($DBH, $query, $params);
-            $oCodes = array();
-            foreach ($theCodes as $aCode) {
-                $oCodes[$aCode[code]] = $aCode[result];
-            }
-            $out[$var] = $oCodes;
-        }
-
-        //  reportToFile("Codes! " . print_r($out, true));
-        break;
+//    case 'getDecoderInfo':
+//        reportToFile("[$command]......." . date("Y-m-d H:i:s (T)"));
+//        $params = array();
+//
+//        $query = "SELECT DISTINCT varname FROM decoder";
+//        $theVars = CODAP_MySQL_getQueryResult($DBH, $query, $params);
+//        $out = array();
+//
+//        foreach ($theVars as $arrayOfValues) {
+//            //  reportToFile('get Decoder -- looking at array: ' . print_r($arrayOfValues, true));
+//            $var = $arrayOfValues[varname];
+//            //  reportToFile('get Decoder -- looking at var: ' . print_r($var, true));
+//            $query = "SELECT * FROM decoder WHERE varname = '".$var . "'";
+//            $theCodes = CODAP_MySQL_getQueryResult($DBH, $query, $params);
+//            $oCodes = array();
+//            foreach ($theCodes as $aCode) {
+//                $oCodes[$aCode[code]] = $aCode[result];
+//            }
+//            $out[$var] = $oCodes;
+//        }
+//
+//        //  reportToFile("Codes! " . print_r($out, true));
+//        break;
 }
 
 $jout = json_encode($out);
