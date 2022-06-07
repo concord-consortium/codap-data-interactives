8422409945f40a6e9ff25112dd46ac59ec82003c
Jonathan Sandoe <jsandoe@concord.org>
2022-06-07 11:01:13 -0700

    [#182338474] Fix bug: "Start over" button in onboarding plugin doesn't work.

--- onboarding/source/jsx/task_descriptions.jsx
+++ onboarding/source/jsx/task_descriptions.jsx
@@ -106,7 +106,7 @@ allAccomplishedFeedback = <div>
   <p>You can do a <em>lot</em> with just those five skills!</p>
   <p>For more information about how to work with CODAP, visit
     the <a href="https://codap.concord.org/help/" target="_blank">CODAP Help</a> page. </p>
-  <button onClick={this.startOver}>Start Over</button>
+  <button onClick={() => window.parent.location.reload()}>Start Over</button>
 </div>;
 
 infoFeedback =
