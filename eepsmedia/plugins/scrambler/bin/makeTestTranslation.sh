52ce1691a3600549921e3a53cf5f44ffaedcace5
Jonathan Sandoe <jsandoe@concord.org>
2022-08-09 15:30:18 -0700

    [#182844929] Scrambler: Scripts to manage strings file.

--- eepsmedia/plugins/scrambler/bin/makeTestTranslation.sh
+++ eepsmedia/plugins/scrambler/bin/makeTestTranslation.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
+# Amends a strings file with a test pseudo-locale.
+# The test locale is enclish strings with "XXX" prepended.
 PROGNAME=`basename $0`
 DIRNAME=`dirname $0`
 
-STRINGS_FILE="./src/strings.json"
-TEMP_FILE=/tmp/$PROGNAME.$$
-JQ=${DIRNAME}/../node_modules/node-jq/bin/jq
+STRINGS_FILE="${DIRNAME}/../src/strings/strings.json"
+TEMP_FILE="/tmp/$PROGNAME.$$"
+JQ="${DIRNAME}/../node_modules/node-jq/bin/jq"
 
-cat $STRINGS_FILE |
+cat "$STRINGS_FILE" |
   $JQ 'del(.test)' |
-  $JQ '. + {"test": (."en-US"|with_entries(.value |= "XXX"+.))}' > $TEMP_FILE
+  $JQ '. + {"test": (."en"|with_entries(.value |= "XXX"+.))}' > $TEMP_FILE
 
 mv $TEMP_FILE $STRINGS_FILE
