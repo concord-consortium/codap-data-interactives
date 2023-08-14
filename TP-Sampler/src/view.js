74bf215dfcad29932d1b8003c5aae999b478c77a
lublagg <lvblagg@gmail.com>
2023-08-14 11:09:33 -0400

    Fix: variable name updates.

--- TP-Sampler/src/view.js
+++ TP-Sampler/src/view.js
@@ -97,10 +97,10 @@ var width = 205,            // svg units
           pctLabelLoc = getCoordinatesForPercent(radius * 1.35, labelPerc2),
           deleteBtnLocY;
 
-      if (labelLocation[1] >= labelP2[1]) {
-        deleteBtnLocY = labelLocation[1] + 15;
+      if (labelLineP1[1] >= labelLineP2[1]) {
+        deleteBtnLocY = labelLineP1[1] + 15;
       } else {
-        deleteBtnLocY = labelLocation[1] - 17;
+        deleteBtnLocY = labelLineP1[1] - 17;
       }
 
       return {
@@ -111,7 +111,7 @@ var width = 205,            // svg units
         },
         labelLine: `M ${labelLineP1.join(" ")} L ${labelLineP2.join(" ")}`,
         pctLabelLoc: {x: pctLabelLoc[0], y: pctLabelLoc[1]},
-        deleteBtnLoc:  {x: labelLocation[0], y: deleteBtnLocY}
+        deleteBtnLoc:  {x: labelLineP1[0], y: deleteBtnLocY}
       };
     },
 
