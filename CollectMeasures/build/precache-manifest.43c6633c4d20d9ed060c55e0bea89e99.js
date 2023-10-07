2cc1b03cb0b4fd6be9f2e233a95ccda9b5616610
eireland <eireland@concord.org>
2023-10-07 08:58:58 -0230

    Build CollectMeasures

---
+++ CollectMeasures/build/precache-manifest.43c6633c4d20d9ed060c55e0bea89e99.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ef198cc12ec02951247",
+    "url": "./static/css/main.ae58ab1b.chunk.css"
+  },
+  {
+    "revision": "0ef198cc12ec02951247",
+    "url": "./static/js/main.0ef198cc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "73e77fe574d408ce9429",
+    "url": "./static/css/2.fee577bf.chunk.css"
+  },
+  {
+    "revision": "73e77fe574d408ce9429",
+    "url": "./static/js/2.73e77fe5.chunk.js"
+  },
+  {
+    "revision": "ecaddb09844ad461ecac777995545eed",
+    "url": "./static/media/dxicons.ecaddb09.woff2"
+  },
+  {
+    "revision": "b51d1bb8df912c9a72f25db27706b9e8",
+    "url": "./static/media/dxicons.b51d1bb8.ttf"
+  },
+  {
+    "revision": "b4f8a75314de92e91d6e29ff27abe9cd",
+    "url": "./static/media/dxicons.b4f8a753.woff"
+  },
+  {
+    "revision": "983852adab02e0b702f6209d96c36f63",
+    "url": "./index.html"
+  }
+];
---
+++ DayLength/build/precache-manifest.7e5f5c24859f7c4bc3b601e28643be0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a64a68b5d5d6fdfeb31",
+    "url": "./static/css/main.e884dbc4.chunk.css"
+  },
+  {
+    "revision": "6a64a68b5d5d6fdfeb31",
+    "url": "./static/js/main.6a64a68b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "0d459b82df364e668a7d",
+    "url": "./static/js/2.0d459b82.chunk.js"
+  },
+  {
+    "revision": "d89da15beebfb0fee992125f902d6d46",
+    "url": "./index.html"
+  }
+];
