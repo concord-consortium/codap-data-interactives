04c62c6c08534e6b339f632cbeb89e69a0074215
lublagg <lvblagg@gmail.com>
2023-10-10 11:48:17 -0400

    Increment version number.

--- TP-Sampler/src/codap-com.js
+++ TP-Sampler/src/codap-com.js
@@ -85,7 +85,7 @@ CodapCom.prototype = {
     return codapInterface.init({
       name: this.localeMgr.tr('DG.plugin.Sampler.title'),
       title: this.localeMgr.tr('DG.plugin.Sampler.title'),
-      version: 'v0.33 (#' + window.codapPluginConfig.buildNumber + ')',
+      version: 'v0.34 (#' + window.codapPluginConfig.buildNumber + ')',
       preventDataContextReorg: false,
       stateHandler: this.loadStateFunc
     }).then( function( iInitialState) {
