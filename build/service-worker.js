f0c5d4d2678450e36708fb9edf7c4fb3ec91c98d
eireland <eireland@concord.org>
2024-01-17 16:20:19 -0800

    update build and re-order data_interactive_map to Sonify above story builder

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.50006c56e19073402d72ae675adcab72.js"
+  "./precache-manifest.b78c79a36dd4399b008eaae85bc3ded5.js"
 );
 
 self.addEventListener('message', (event) => {
